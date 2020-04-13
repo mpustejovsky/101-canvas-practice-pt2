878a4fe79161e9735555ccdb87dea88d19e5e6b0
HippErger <berger.clayton@gmail.com>
2020-04-13 13:02:25 -0500

    added instructions and started code:

--- main.js
+++ main.js
@@ -1,24 +1,54 @@
+// define the canvas
 let canvas = document.getElementById("my-canvas")
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const c = canvas.getContext('2d')
-
 // console.log(canvas)
 
+// adjust the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-window.onclick = () => {
-  for(let i=0; i < 50; i++ ) {
-    const x = Math.random() * (window.innerWidth - 100)
-    const y = Math.random() * (window.innerHeight - 100)
-    // the first value will be null to accommodate for no 0 number being drawn
-    const colors = [null, "#8C0C3C", "#1B2968", "#4B9C2B", "#A4C89C", "#F8605F", "#F8B493", "#32B9B2", "#F85532", "#C2C8E4", "#357153", "#A061D4", "#404462"]
-    const randomIndex = Math.floor(Math.random() * (13 - 1)) + 1;
+// capture the drawing context in a variable
+const c = canvas.getContext('2d')
 
-    // Draw Circle
-    c.beginPath();
-    // replace "black" with the random color selected from the list
-    c.strokeStyle = colors[randomIndex];
-    c.arc(x, y, 50, 0, 2 * Math.PI);
-    c.stroke();
-  }
+// draw a circle
+c.beginPath();
+c.strokeStyle = "limegreen";
+c.arc(800, 400, 50, 0, 2 * Math.PI);
+c.stroke();
+
+// TODO follow the instructions in the Canvas Pt. 2 Pre-Lesson to see how to build this project
+const animate = () => {
+
 }
+
+// animate()
+
+
+
+
+
+
+
+
+
+
+
+
+// Code to draw new circles on a click of the window
+// *************************************************
+
+// window.onclick = () => {
+//   for(let i=0; i < 50; i++ ) {
+//     const x = Math.random() * (window.innerWidth - 100)
+//     const y = Math.random() * (window.innerHeight - 100)
+//     // the first value will be null to accommodate for no 0 number being drawn
+//     const colors = [null, "#8C0C3C", "#1B2968", "#4B9C2B", "#A4C89C", "#F8605F", "#F8B493", "#32B9B2", "#F85532", "#C2C8E4", "#357153", "#A061D4", "#404462"]
+//     const randomIndex = Math.floor(Math.random() * (13 - 1)) + 1;
+
+//     // Draw Circle
+//     c.beginPath();
+//     // replace "black" with the random color selected from the list
+//     c.strokeStyle = colors[randomIndex];
+//     c.arc(x, y, 50, 0, 2 * Math.PI);
+//     c.stroke();
+//   }
+// }
